de780485cdea2e16e753229d622195c2fa98bec6
VipulD <vipuld04@gmail.com>
2023-08-14 21:49:29 +0530

    commnets display

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -5,7 +5,16 @@ const Post = require("../models/post");
 module.exports.home = async function (req, res) {
   // console.log(req.user);
   try {
-    const posts = await Post.find({}).populate("user").exec();
+    const posts = await Post.find({})
+      .populate("user") // Populate the user field in the Post model
+      .populate({
+        path: "comments",
+        populate: {
+          path: "user", // Populate the user field in the Comment model
+        },
+      })
+      .exec();
+
     return res.render("home", {
       title: "Codial | Home",
       posts: posts,
