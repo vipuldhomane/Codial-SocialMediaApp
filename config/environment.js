9c10662a0ab1f8d895c82ec78ced8f274672ea76
VipulD <vipuld04@gmail.com>
2023-09-20 19:42:10 +0530

    mis

--- config/environment.js
+++ config/environment.js
@@ -29,8 +29,8 @@ const development = {
   google_client_id:
     "507267631010-p1hu0cth0oje04oa6q7olcb7d6r5j7ca.apps.googleusercontent.com",
   google_client_secret: "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",
-  // google_callback_url: "http://localhost:8000/users/auth/google/callback",
-  google_callback_url: "http://13.53.177.135/users/auth/google/callback",
+  google_callback_url: "http://localhost:8000/users/auth/google/callback",
+  // google_callback_url: "http://13.53.177.135/users/auth/google/callback", // AWS ip
   morgan: {
     mode: "dev",
     options: {
@@ -69,11 +69,11 @@ const production = {
   jwt_secret: process.env.CODIAL_JWT_KEY,
 };
 
-module.exports =
-  eval(process.env.NODE_ENV) == undefined
-    ? development
-    : eval(process.env.NODE_ENV);
+// module.exports =
+//   eval(process.env.NODE_ENV) == undefined
+//     ? development
+//     : eval(process.env.NODE_ENV);
 
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
 
-// module.exports = development;
+module.exports = development;
